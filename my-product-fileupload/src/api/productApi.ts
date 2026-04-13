82676b5710eff72cfc0379416043d8e826e46771
redmoondev3000 <redmoon.dev3000@gmail.com>
2026-04-13 11:39:36 +0900

    fix: change api baseURL to relative path

--- my-product-fileupload/src/api/productApi.ts
+++ my-product-fileupload/src/api/productApi.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import type { ProductDTO } from "../types/Product";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080/api/product",
+  baseURL: "/api/product",
   headers: { "Content-Type": "application/json" },
 });
 
